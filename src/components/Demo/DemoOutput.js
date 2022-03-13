838d76d9b0f800fc6ef22528d3c015fb02de151b
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-13 17:38:26 +0100

    comment

--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -6,5 +6,6 @@ const DemoOutput = (props) => {
   return <MyParagraph>{props.show ? "This is new!" : ""}</MyParagraph>;
 };
 
-// this makes that React will not reevaluating this component and it's children
+// this makes that React will not re-evaluating this component and it's children
+// great tool if u have huge component tree u can avoid unnecessary re-evaluation for entire branch
 export default React.memo(DemoOutput);
