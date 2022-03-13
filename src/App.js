c098c43577db925715dd79cfb4f8409ee1ccd720
Patrik Přidal <98656826+vibemane@users.noreply.github.com>
2022-03-13 17:09:58 +0100

    added const "toggleParagraphHandler for showing paragraph when button is clicked

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {showParagraph && <p>This is new!</p>} 
-      <Button>Show Paragraph</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
